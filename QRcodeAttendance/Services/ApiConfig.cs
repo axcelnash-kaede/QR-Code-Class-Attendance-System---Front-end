eb5e037e4929586777a7012a8eac629c2d2d307b
Jhermaine Nheil Emnace <jhernhel.emnace17@gmail.com>
2026-04-13 22:21:51 +0800

    latest

---
+++ QRcodeAttendance/Services/ApiConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace QRcodeAttendance
+{
+    public static class ApiConfig
+    {
+        public const string BaseUrl = "http://192.168.8.147:5041";
+    }
+}
--- QRcodeAttendance/obj/Debug/net8.0/QRcodeAttendance.AssemblyInfo.cs
+++ QRcodeAttendance/obj/Debug/net8.0/QRcodeAttendance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QRcodeAttendance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34cefe4e794126f67319ee31d103a2f08908c075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed9f0bf82afa88d54dcd3dbf97e7171c31cd019")]
 [assembly: System.Reflection.AssemblyProductAttribute("QRcodeAttendance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QRcodeAttendance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
